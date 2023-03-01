307cbd8206579852a290e272195174bff62ddb1c
Anthony Jack <jackie2002jack@gmail.com>
2023-03-01 16:01:49 -0600

    Button Code
    (again)

--- app/src/main/java/com/example/perfect_planner/AddedActivity.java
+++ app/src/main/java/com/example/perfect_planner/AddedActivity.java
@@ -1,7 +1,9 @@
 package com.example.perfect_planner;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,6 +13,11 @@ public class AddedActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.added_main);
+        Button SaveBTN = findViewById(R.id.saveBTN);
+        SaveBTN.setOnClickListener(view ->{
+            Intent i = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(i);
+        });
     }
 
     public class addActivity {
