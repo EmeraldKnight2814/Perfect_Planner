79767fff10156312062ed2294d6056bdfae7a29a
Daniel J Crowley <dcrow2814@gmail.com>
2023-03-01 15:51:43 -0600

    Spinner filled out

--- app/src/main/java/com/example/perfect_planner/MainActivity.java
+++ app/src/main/java/com/example/perfect_planner/MainActivity.java
@@ -5,6 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +16,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner filters = findViewById(R.id.filter);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.filters, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        filters.setAdapter(adapter);
     }
 
     public void addPleaseButton (View v){
