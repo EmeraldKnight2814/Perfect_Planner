e153c8ce955b0cb1ebdc3dd9dd61c3e5e9f65a59
Daniel J Crowley <dcrow2814@gmail.com>
2023-03-01 16:07:06 -0600

    startActivity on add button

--- app/src/main/java/com/example/perfect_planner/MainActivity.java
+++ app/src/main/java/com/example/perfect_planner/MainActivity.java
@@ -25,6 +25,6 @@ public class MainActivity extends AppCompatActivity {
 
     public void addPleaseButton (View v){
         Intent intent = new Intent(this, AddedActivity.class);
-        mAddNewAssignment.launch(intent);
+        startActivity(intent);
     }
 }
