49d6bed400f345d99d2e2017a21350779e45294d
Anthony Jack <jackie2002jack@gmail.com>
2023-03-22 16:12:35 -0500

    Update plannerAdapter.java
    Boiler Plate Adapter code
    Added the basic functions

--- app/src/main/java/com/example/perfect_planner/plannerAdapter.java
+++ app/src/main/java/com/example/perfect_planner/plannerAdapter.java
@@ -1,4 +1,32 @@
 package com.example.perfect_planner;
 
-public class plannerAdapter {
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class plannerAdapter extends RecyclerView.Adapter<plannerAdapter.PlannerViewHolder> {
+    @NonNull
+    @Override
+    public PlannerViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PlannerViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public static class PlannerViewHolder extends RecyclerView.ViewHolder{
+
+        public PlannerViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
