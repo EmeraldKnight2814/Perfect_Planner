fa1df01ac09f1ae24a3fa8662462eebecb42b14e
haebin827 <99354243+haebin827@users.noreply.github.com>
2023-03-22 16:26:06 -0500

    Added codes for Model class (arraylist, new inner class, etc)
    S538108

--- app/src/main/java/com/example/perfect_planner/Model.java
+++ app/src/main/java/com/example/perfect_planner/Model.java
@@ -1,5 +1,60 @@
 package com.example.perfect_planner;
 
-public class Model {
+import java.util.ArrayList;
 
+public class Model {
+    private static Model theModel = null;
+    private ArrayList<Task> taskList;
+
+    private Model() {
+        taskList = new ArrayList<Task>();
+    }
+
+    public static Model getModel() {
+        if (theModel == null) {
+            theModel = new Model();
+        }
+        return theModel;
+    }
+
+    public ArrayList<Task> getTaskList() {
+        return taskList;
+    }
+
+    public void newPage(int position) {
+        //add a code to open a new activity layout
+    }
+
+    public static class Task {
+        private String assignment;
+        private int date;
+
+        public Task(String assignment, int date) {
+            this.assignment = assignment;
+            this.date = date;
+        }
+
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
+        public int getCount() {
+            return count;
+        }
+        public void setCount(int count) {
+            this.count = count;
+        }
+    }
+
+    public void clear() {
+        taskList.clear();
+    }
+
+    public void zeroCount() {
+        for (Task task : taskList) {
+            task.setCount(0);
+        }
+    }
 }
