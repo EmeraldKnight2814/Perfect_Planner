cf9aae9cfec1de2be3ac2bf257ceb39c36e41a2f
Anthony Jack <jackie2002jack@gmail.com>
2023-04-06 14:12:31 -0500

    DatePicker
    Date picker changed from a edit text to a calendar popup that you can pick the date.
    - Anthony

--- app/src/main/java/com/example/perfect_planner/AddedActivity.java
+++ app/src/main/java/com/example/perfect_planner/AddedActivity.java
@@ -1,16 +1,20 @@
 package com.example.perfect_planner;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
 
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class AddedActivity extends AppCompatActivity {
 
@@ -20,6 +24,26 @@ public class AddedActivity extends AppCompatActivity {
         setContentView(R.layout.added_main);
         Button SaveBTN = findViewById(R.id.saveBTN);
         Button CancelBTN = findViewById(R.id.cancelBTN);
+        Button datePicker = findViewById(R.id.datePickerBTN);
+        TextView dateTV = findViewById(R.id.dateTV);
+        String date = "";
+        datePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Calendar c = Calendar.getInstance();
+                int year = c.get(Calendar.YEAR);
+                int month = c.get(Calendar.MONTH);
+                int day = c.get(Calendar.DAY_OF_MONTH);
+                DatePickerDialog datePickerDialog = new DatePickerDialog(AddedActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year,
+                                          int monthOfYear, int dayOfMonth) {
+                        dateTV.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+                    }
+                }, year, month, day);
+                datePickerDialog.show();
+            }
+        });
         CancelBTN.setOnClickListener(view ->{
             Intent i = new Intent(getApplicationContext(), MainActivity.class);
             startActivity(i);
@@ -28,9 +52,9 @@ public class AddedActivity extends AppCompatActivity {
     public void createAssignment (View v){
         EditText assignName = findViewById(R.id.nameET);
         Spinner catClass = findViewById(R.id.categoryET);
-        EditText dueDate = findViewById(R.id.dueDateET);
+        TextView dateTV = findViewById(R.id.dateTV);
 
-        Model.Asgmt asgmnt = new Model.Asgmt(assignName.getText().toString(), dueDate.getText().toString(), catClass.getSelectedItem().toString());
+        Model.Asgmt asgmnt = new Model.Asgmt(assignName.getText().toString(), dateTV.getText().toString(), catClass.getSelectedItem().toString());
         ArrayList<Model.Asgmt> list = Model.getModel().getAsgmtList();
 
         boolean contained = false;
