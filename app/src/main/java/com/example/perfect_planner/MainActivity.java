02011a0f964a9f6ca477d579cc8fa4cc1cd2e8ea
haebin827 <99354243+haebin827@users.noreply.github.com>
2023-04-13 14:16:33 -0500

    Made some changes

--- app/src/main/java/com/example/perfect_planner/MainActivity.java
+++ app/src/main/java/com/example/perfect_planner/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.perfect_planner;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
