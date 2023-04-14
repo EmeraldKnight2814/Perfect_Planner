330f172d68aa8ac79fc99ab04b7daef0179cb5f3
haebin827 <99354243+haebin827@users.noreply.github.com>
2023-04-14 13:37:30 -0500

    Added instruction button & InstructionActivity & instruction_main layout and added intent code

--- app/src/main/java/com/example/perfect_planner/MainActivity.java
+++ app/src/main/java/com/example/perfect_planner/MainActivity.java
@@ -70,6 +70,11 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    public void instructionButton (View v){
+        Intent intent = new Intent(getApplicationContext(), InstructionActivity.class);
+        startActivity(intent);
+    }
+
     public void editButton(View v){
         //get which assignment was clicked
         ArrayList<Model.Asgmt> assignments = Model.getModel().getAsgmtList();
@@ -99,7 +104,4 @@ public class MainActivity extends AppCompatActivity {
      */
 
 
-    public void instruction(View v){
-        Toast.makeText(getApplicationContext(),"Our app the Perfect Planner seeks to keep you organized.\nThis app will provide a place to store your assignments, the due date for said assignments, the details for assignments, and a place to mark it as done!", Toast.LENGTH_LONG).show();
-    }
 }
