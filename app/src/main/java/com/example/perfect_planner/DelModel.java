137e60b4015bb587387b2dd35be7cf42b808463c
Daniel J Crowley <dcrow2814@gmail.com>
2023-04-14 13:27:40 -0500

    added new model and adapter for recently deleted
    If an item is deleted from the model, it will go into this model temporarily. Unlike the main model, this del model will not have persistent storage, as it is meant to serve as a temporary "oops I deleted something I didn't want to" failsafe.

---
+++ app/src/main/java/com/example/perfect_planner/DelModel.java
@@ -0,0 +1,43 @@
+package com.example.perfect_planner;
+
+import java.util.ArrayList;
+
+public class DelModel {
+    private static DelModel theModel = null;
+    private ArrayList<Model.Asgmt> asgmtList;
+
+    private DelModel() {
+        asgmtList = new ArrayList<Model.Asgmt>();
+    }
+
+    public static DelModel getModel() {
+        if (theModel == null) {
+            theModel = new DelModel();
+        }
+        return theModel;
+    }
+
+    public ArrayList<Model.Asgmt> getAsgmtList() {
+        return asgmtList;
+    }
+
+    public void clear() {
+        asgmtList.clear();
+    }
+
+    public void removeItem(int position) {
+        asgmtList.remove(position);
+    }
+
+    public void restoreItem(int position){
+        Model.getModel().getAsgmtList().add(asgmtList.get(position));
+        asgmtList.remove(position);
+    }
+
+    public void restoreAll(){
+        for(int i = 0; i < asgmtList.size(); i++){
+            Model.getModel().getAsgmtList().add(asgmtList.get(i));
+        }
+        asgmtList.clear();
+    }
+}
